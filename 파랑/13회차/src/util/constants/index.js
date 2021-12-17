c0eb039f6421229ee0b3abd1c7cd14e54a733455
InSeongSo <goflvhxj2547@gmail.com>
2021-12-17 16:01:44 +0900

    ✒️Feat: 13회차 페어 프로그래밍에 앞선 준비

---
+++ 파랑/13회차/src/util/constants/index.js
@@ -0,0 +1,20 @@
+// 디스플레이 최대 입력 개수
+export const LIMIT_DISPLAY_LENGTH = 69;
+// 피연산자 최대 입력 개수
+export const LIMIT_OPERAND_LENGTH = 15;
+
+export const OPERATORS_SET = new Set(['+', '-', 'x', '/', '%']);
+
+/**
+ * 정규표현식
+ */
+export const REGEXP_DIGIT = /\d/; // 임시
+// export const REGEXP_DIGIT = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+export const REGEXP_OPERATOR = /(?<=\d)([+\-x/%])/g;
+export const REGEXP_REMOVED_ZERO = /^0(?![.])/gm;
+export const REGEXP_REMOVED_ZERO_DECIMAL = /^[0.]*(?<![1-9])|[.]/gm;
+
+export const EMPTY = '';
+export const DECIMAL = '.';
+export const OPERATOR = 'operator';
+export const DIGIT = 'digit';
