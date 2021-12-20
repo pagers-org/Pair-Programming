b1aaf97e372fc82fd60694de28367bcaa1007064
InSeongSo <goflvhxj2547@gmail.com>
2021-12-20 22:35:54 +0900

    ✒️Feat: 14회차 페어 프로그래밍 추가

---
+++ pairs/13회차/README.md
@@ -0,0 +1,20 @@
+# 타이머
+
+> 파랑: 미리 본 거
+  > - 어떻게 구현할지: 타이머 = `setInterval`, `clearInterval`, `Component`
+  > - 폰트: **clockicons**
+
+<br>
+
+> 아벤님: 생각해 오신 거
+  > - 어떤 기능 : 타이머, 참가자 input 태그 입력/출력, 역할 영역
+
+
+구현할 수 있는 사항 : 외주/개발자
+||
+Gap 분석 > 개발
+||
+요구사항 : 현업
+
+Linter 설정하기
+- Vue, Typescript 환경에서 어떻게 ?
---
+++ pairs/14회차/src/components/main/progressbar/Progressbar.js
@@ -0,0 +1,18 @@
+const Progressbar = () => {
+  return `
+        <section class="progressbar-wrap">
+            <div class="roles">
+                <div class="driver">
+                    <span class="driver-name">파랑</span>
+                    <span class="driver-icon">🚗</span>
+                </div>
+                <div class="navigator">
+                    <span class="navigator-name">아벤</span>
+                    <span class="navigator-icon">😎</span>
+                </div>
+            </div>
+            <div class="progressbar"></div>
+        </section>
+      `;
+};
+export default Progressbar;
